511a2123d2fce44ff894975c16a777dae34626fa
jinseok9338 <jinseok9338@gmail.com>
2021-12-26 14:11:35 +0000

    minopr change

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -2,7 +2,7 @@ import { Resolver, Query } from '@nestjs/graphql';
 import { Pet } from './pet.entity';
 import { PetsService } from './pets.service';
 
-@Resolver((of) => Pet)
+@Resolver((_of) => Pet)
 export class PetsResolver {
   constructor(private petsService: PetsService) {}
 
