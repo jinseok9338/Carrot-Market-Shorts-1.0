6ef0612613557e1800057da079008c2f7a3b1a5e
jinseok9338 <jinseok9338@gmail.com>
2021-12-29 14:15:05 +0000

    something wrong with ubuntu server

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { PetsModule } from './pets/pets.module';
 import { join } from 'node:path/win32';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+console.log(process.env.DATABASE_URL);
+
 @Module({
   imports: [
     GraphQLModule.forRoot({
@@ -23,10 +25,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       //     rejectUnauthorized: false,
       //   },
       // },
-      url: 'process.env.DATABASE_URL',
+      url: 'postgres://iegootncokyvsa:8b69632c19af9b2dc418eceb782d8153b36cdee0e27381d6315b2c3a5bed4152@ec2-54-198-213-75.compute-1.amazonaws.com:5432/ddio4kogtjt097',
       synchronize: true, // In Prod use Migration
       entities: ['dist/**/*/entity{.ts,.js}'],
-      ssl: true,
     }),
     PetsModule,
   ],
