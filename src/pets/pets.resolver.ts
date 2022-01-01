07b1a495f43ea12c43f68d35d043685c42f36096
jinseok9338 <jinseok9338@gmail.com>
2022-01-01 09:35:02 +0000

    added resolver pets

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -1,7 +1,16 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  Parent,
+  ResolveField,
+} from '@nestjs/graphql';
 import { Pet } from './pet.entity';
 import { PetsService } from './pets.service';
 import { CreatePetInput } from './dto/createPet.input';
+import { Owner } from 'src/owners/entities/owner.entity';
 
 @Resolver((_of) => Pet)
 export class PetsResolver {
@@ -15,6 +24,11 @@ export class PetsResolver {
   @Query((returns) => [Pet])
   pets(): Promise<Pet[]> {
     return this.petsService.findAll();
+  }
+
+  @ResolveField((returns) => Owner)
+  owner(@Parent() pet: Pet): Promise<Owner> {
+    return this.petsService.getOwner(pet.ownerId);
   }
 
   @Mutation((returns) => Pet)
