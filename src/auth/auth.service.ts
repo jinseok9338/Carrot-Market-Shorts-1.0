fddacb68c1b49f451bc8e9fe69d9618f6503f8f0
jinseok9338 <jinseok9338@gmail.com>
2022-01-12 11:23:32 +0000

    Token Error Finish it later tonight

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,7 +10,14 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
+  async validateUser(email: string, password: string): Promise< {
+    id: number;
+    userId: string;
+    email?: string;
+    firstName?: string;
+    lastName?: string;
+    confirmEmail?: boolean;
+} |null> {
     const user = await this.usersService.findOne(email);
     // if user is not present return null ... or ... error...
     if (!user) return null;
@@ -23,12 +30,46 @@ export class AuthService {
     return null;
   }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.userId }; // ?
+  async login(user: {email:string, password:string}){
+    const userProfile = await this.validateUser(user.email, user.password)
+    const payload = {...userProfile};
     return {
       access_token: this.jwtService.sign(payload),
     };
   }
+
+  async signUp(signUpUser: {
+    id: number;
+    userId: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    confirmEmail: boolean;
+    password: string;
+    passwordConfirm: string;
+}) { // I need to mark it types 
+    
+    const { password, passwordConfirm ,...result } = signUpUser;
+
+       // Create User in the dataBase then return access_token
+    
+       try{
+        const user = await this.usersService.createUser(signUpUser);
+
+        if(user.user){
+          user.user.password = ""
+        }
+
+        return {...user,
+          access_token: this.jwtService.sign(result),
+        };
+       }
+       catch(e){
+         return {ok:false, error:e.message}
+       }
+   
+  }
+
 
   getHello(): string {
     return 'Hello 3!';
