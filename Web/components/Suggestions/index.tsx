4f1c7d15760f6ba192cf50db8c6c42373c80a17b
jinseok9338 <jinseok9338@gmail.com>
2022-01-22 13:24:23 +0000

    Making all the components

--- Web/components/Suggestions/index.tsx
+++ Web/components/Suggestions/index.tsx
@@ -1,9 +1,80 @@
 import { FC } from "react";
-interface ISuggestionsProps {}
+import Button from "../Button";
+import { RecommendCard } from "../RecommendCard";
+import User from "../User";
+import { BoxContainer } from "./BoxContainer";
+interface ISuggestionsProps {
+  people: any;
+  items: any;
+}
 
-export const Suggestions: FC<ISuggestionsProps> = (props) => {
-  return <div></div>;
+// export const Icon = styled.img`
+//   width: 31px;
+//   height: 31px;
+//   margin-right: 15px;
+// `;
+
+const Icon = ({ src }: { src: string }) => (
+  <img src={src} className="w-[31px] h-[31px] mr-[15px]"></img>
+);
+
+const DownloadImage = ({ src }: { src: string }) => (
+  <img src={src} className="w-[170px] h-[50px] rounded-[5px] mb-[12px]"></img>
+);
+
+export const Suggestions: FC<ISuggestionsProps> = ({ people, items }) => {
+  return (
+    <div className="Suggestion-container flex-[0_0_330px] w-[330px] ml-[24px] xl:hidden">
+      <div className="icon-container mb-[39px] text-left">
+        <Icon src="/images/facebookIcon.svg"></Icon>
+        <Icon src="/images/pinterestIcon.svg"></Icon>
+        <Icon src="/images/twitterIcon.svg"></Icon>
+      </div>
+      <BoxContainer boxHeaderText="Contas sugeridas">
+        {people.map((person: any, index: any) => (
+          <div className="flex items-center mb-[20px]">
+            <User key={index} user={person}></User>
+            <div className="w-[95px] h-[28px] font-[600] text-[16px] ml-[auto]">
+              <Button fontSize={14} outlined>
+                Seguir
+              </Button>
+            </div>
+          </div>
+        ))}
+      </BoxContainer>
+      <BoxContainer boxHeaderText="Descobrir">
+        {items.map((item: any, index: any) => (
+          <div className="flex items-center mb-[20px]">
+            <RecommendCard key={index} recommend={item}></RecommendCard>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="arrowIcon ml-[auto] h-[30px] w-[14px]"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
+        ))}
+      </BoxContainer>
+      <DownloadImage src="/images/appstore.png"></DownloadImage>
+      <DownloadImage src="/images/playstore.png"></DownloadImage>
+      <DownloadImage src="/images/amazon.png"></DownloadImage>
+    </div>
+  );
 };
+
+// export const ArrowIcon = styled(ArrowForwardIos)`
+//   margin-left: auto;
+//   height: 20px;
+//   width: 14px;
+// `;
 
 // export const Container = styled.div`
 //   flex: 0 0 330px;
@@ -23,30 +94,6 @@ export const Suggestions: FC<ISuggestionsProps> = (props) => {
 //   width: 31px;
 //   height: 31px;
 //   margin-right: 15px;
-// `;
-
-// export const BoxContainer = styled.div`
-//   position: relative;
-//   border: 1px solid rgba(22, 24, 35, 0.12);
-//   border-radius: 8px;
-//   height: 380px;
-//   display: flex;
-//   flex-direction: column;
-//   margin-bottom: 55px;
-// `;
-
-// export const BoxHeader = styled.div`
-//   padding: 24px 16px;
-//   padding-right: 12px;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: baseline;
-//   flex: 0 0 auto;
-//   > span {
-//     font-weight: 700;
-//     line-height: 1;
-//     font-size: 24px;
-//   }
 // `;
 
 // export const ButtonContainer = styled.div`
@@ -80,12 +127,6 @@ export const Suggestions: FC<ISuggestionsProps> = (props) => {
 //   font-weight: 400;
 //   font-size: 14px;
 //   color: rgba(22, 24, 35, 0.5);
-// `;
-
-// export const ItemContainer = styled.div`
-//   height: calc(100% - 72px);
-//   overflow: auto;
-//   cursor: pointer;
 // `;
 
 // export const ArrowIcon = styled(ArrowForwardIos)`
