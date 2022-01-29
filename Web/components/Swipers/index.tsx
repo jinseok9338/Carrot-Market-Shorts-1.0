407ee51d3fb7dd9dff1ff646f2bf12001376caa5
jinseok9338 <jinseok9338@gmail.com>
2022-01-29 13:10:57 +0000

    experiment with spring react

--- Web/components/Swipers/index.tsx
+++ Web/components/Swipers/index.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react";
-import { useSprings } from "react-spring/hooks";
-import { useGesture } from "react-with-gesture";
+import { useSpring, animated } from "@react-spring/web";
+import { useDrag } from "@use-gesture/react";
 
 interface ISwiperProps {}
 
 export const SwiperView: FC<ISwiperProps> = (props) => {
-  return <></>;
+  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
+
+  // Set the drag hook and define component movement based on gesture data
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down });
+  });
+  return (
+    <animated.div {...bind()} style={{ x, y }}>
+      <p>Hello?</p>
+    </animated.div>
+  );
 };
