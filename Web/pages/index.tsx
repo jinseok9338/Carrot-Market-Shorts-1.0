6fc33756cd15f3ad07d8ae24e6b433f4f9c1f119
jinseok9338 <jinseok9338@gmail.com>
2022-02-03 13:30:51 +0000

    Weird....

--- Web/pages/index.tsx
+++ Web/pages/index.tsx
@@ -4,20 +4,19 @@ import Footer from "../components/Footer";
 import { SwiperView } from "../components/SwipeableVideo";
 import { ProvideAuth, useAuth } from "../utils/auth/useAuth";
 import LoginPage from "./Login";
+import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   const auth = useAuth();
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  useEffect(() => {
-    if (auth?.user) {
-      setIsAuthenticated(true);
-    }
-  }, [auth?.user]);
+
+  // const { data: session, status } = useSession();
+  // const loading = status === "loading";
+
   console.log(auth?.user);
   return (
     <ProvideAuth>
       <div className="home">
-        {isAuthenticated ? (
+        {auth?.user ? (
           <>
             <SwiperView />
             <Footer />
