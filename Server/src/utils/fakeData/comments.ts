d1abe05f5a4a7eb3e8d2fa03f3f2f1a484c38e7f
jinseok9338 <jinseok9338@gmail.com>
2022-02-11 11:29:29 +0000

    create cOmments test Data

--- Server/src/utils/fakeData/comments.ts
+++ Server/src/utils/fakeData/comments.ts
@@ -1,8 +1,23 @@
 import { Comment } from 'src/comment/entities/comment.entity';
+import { uuid } from 'uuidv4';
+import { lorem } from 'faker';
 
 export const createComments = (
   user_ids: string[],
   product_id: string,
 ): Comment[] => {
-  return;
+  let comments = [];
+  for (let i = 0; i < user_ids.length; i++) {
+    let comment: Comment = {
+      comment_id: uuid(),
+      created_at: new Date(),
+      message: lorem.sentence(),
+      product_id: product_id,
+      updated_at: new Date(),
+      user_id: user_ids[i], // There is no user name...
+    };
+    comments.push(comment);
+  }
+
+  return comments;
 };
