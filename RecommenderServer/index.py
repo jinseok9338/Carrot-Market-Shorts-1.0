569aa449665748aaafb73801d6f3d8d4eb411ebb
jinseok9338 <jinseok9338@gmail.com>
2022-02-20 11:12:52 +0000

    Setting Up Flask Server

---
+++ RecommenderServer/index.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/flasksql'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'secret string'
+
+
+@app.route('/')
+def hello(): 
+    return 'Hello, World!'
