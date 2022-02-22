079ad6782166e3c32b36a862a5df938caa8e6966
jinseok9338 <jinseok9338@gmail.com>
2022-02-22 13:14:09 +0000

    Change the version of conda

--- RecommenderServer/get.sh
+++ RecommenderServer/get.sh
@@ -1,1 +1,1 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -P /workspace/Carrot-Market-Shorts-1.0/RecommenderServer
+wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.10.3-Linux-x86_64.sh -P /workspace/Carrot-Market-Shorts-1.0/RecommenderServer
