69628dfec3ccd30bee9d65ddc8dab267055eddc2
jinseok9338 <jinseok9338@gmail.com>
2022-02-23 13:50:21 +0000

    Connecting the postGres

--- RecommenderServer/FlaskServer/Database/models.py
+++ RecommenderServer/FlaskServer/Database/models.py
@@ -6,14 +6,3 @@ from app import app
 
 db = SQLAlchemy()
 
-POSTGRES = {
-    'user': 'user',
-    'db': 'mydb',
-    'host': 'localhost',
-    'port': '5432',
-}
-
-postGresUri = 'postgresql://%(user)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-print(postGresUri)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\s@%(host)s:%(port)s/%(db)s' % POSTGRES
--- RecommenderServer/FlaskServer/app.py
+++ RecommenderServer/FlaskServer/app.py
@@ -1,8 +1,21 @@
 from flask import Flask
-
+from models import db
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+#PostGres Config
+POSTGRES = {
+    'user': 'user',
+    'db': 'mydb',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+postGresUri = 'postgresql://%(user)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = postGresUri
 
 @app.route("/")
 def hello():
