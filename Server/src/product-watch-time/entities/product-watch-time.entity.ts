27f851fe7b1c5b1c6d05736ba0b1cd68067fcf80
jinseok9338 <jinseok9338@gmail.com>
2022-02-28 14:35:55 +0000

    Making Product watch Time

--- Server/src/product-watch-time/entities/product-watch-time.entity.ts
+++ Server/src/product-watch-time/entities/product-watch-time.entity.ts
@@ -1,7 +1,27 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Product } from 'src/products/entities/product.entity';
 
+import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
+
+@Entity()
 @ObjectType()
-export class ProductWatchTime {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class UserWatchTime {
+  @PrimaryColumn()
+  @Field(() => String)
+  product_watch_time_id: string;
+
+  @Column('simple-array')
+  @Field(() => [String])
+  user_ids: string[];
+
+  @Column()
+  @Field(() => [UserWatchTime])
+  user_watch_times: UserWatchTime[];
+
+  @Field(() => Int) // Resolved Field
+  total_seconds: number;
+
+  @Column()
+  @Field(() => Product)
+  product: Product;
 }
