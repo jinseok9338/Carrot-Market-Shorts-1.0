bc5494ca6a6038fec6f803b6eae978aaf2f11cbc
jinseok9338 <jinseok9338@gmail.com>
2022-03-06 00:02:19 +0000

    Making Me Page

--- Web/components/Me/index.tsx
+++ Web/components/Me/index.tsx
@@ -3,20 +3,26 @@ interface IMeProps {}
 
 // we need to make profile page that's scrollable ...
 const Me: FC<IMeProps> = ({}) => {
+  const images = [
+    `https://picsum.photos/seed/${Math.random()}/800/800/?blur=2`,
+    `https://picsum.photos/seed/${Math.random()}/800/800/?blur=2`,
+    `https://picsum.photos/seed/${Math.random()}/800/800/?blur=2`,
+    `https://picsum.photos/seed/${Math.random()}/800/800/?blur=2`,
+  ];
   return (
-    <div className="w-full h-full  flex flex-col items-center justify-start ">
+    <div className=" w-full h-full flex flex-col items-center justify-start ">
       <div className="background h-[60%]">
         <img
           src="https://picsum.photos/800/800/?blur=2"
           className="bg-Profile-background w-full h-full"
         />
       </div>
-      <div className="absolute bottom-[9vh] flex flex-col justify-start items-center  w-full h-[45vh]">
-        <img
-          className="avatar rounded-full w-32 h-32 border-4 border-[white] z-20"
-          src="https://picsum.photos/id/870/200/300?grayscale"
-        />
-        <div className="absolute w-[85vw] h-[28vh] bottom-8 rounded-[20px] z-10 bg-[white] flex flex-col items-center">
+      <img
+        className="absolute avatar top-[40vh] rounded-full w-32 h-32 border-4 border-[white] z-20"
+        src="https://picsum.photos/id/870/200/300?grayscale"
+      />
+      <div className="flex flex-col justify-start items-center  w-full h-[35vh]">
+        <div className=" w-[85vw] h-[28vh] mt-[6vh] rounded-[20px] z-10 bg-[white] flex flex-col items-center">
           <span className="text-[#1E414D] text-[24px] mt-[5vh] leading-[29px] font-Montserrat">
             Jason
           </span>
@@ -58,6 +64,13 @@ const Me: FC<IMeProps> = ({}) => {
           </div>
         </div>
       </div>
+      <div className="devider w-[95%] h-[1px] bg-[black]/[0.7] mb-2"></div>
+      <div className="image-container grid grid-cols-3 ">
+        {images.map((image) => (
+          <img className="cursor-pointer" src={image} />
+        ))}
+      </div>
+      <div className="dummy-bottom bg-[black] w-full h-[10vh]"></div>
     </div>
   );
 };
