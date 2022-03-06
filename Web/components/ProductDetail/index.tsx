defa2ecc3ce815765a8f9abd445ccdf0b02f9d67
jinseok9338 <jinseok9338@gmail.com>
2022-03-06 00:21:47 +0000

    Made Product Detail page

---
+++ Web/components/ProductDetail/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+interface IProdcutDetailProps {}
+
+const ProdcutDetail: FC<IProdcutDetailProps> = (props) => {
+  return <div>Hello World</div>;
+};
+
+export default ProdcutDetail;
